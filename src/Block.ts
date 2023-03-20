c1e0eff56e1e1f6d40354d2c5e79731b28e0eb83
Alyona Korenkovich <al.korenkovich@mail.ru>
2023-03-20 12:23:52 +0300

    #2 Update class Block with `mine` function and `nonce` field

--- src/Block.ts
+++ src/Block.ts
@@ -8,12 +8,12 @@ class Block {
   data: string;
   previousHash: string;
   hash: string;
+  nonce: number;
 
   /**
    * Block constructor
    * @param {number} index
    * @param {string} data
-   * @param {string} previousHash
    */
   constructor({index, data}: TBlock) {
     this.validateIndex(index);
@@ -22,6 +22,7 @@ class Block {
     this.data = data;
     this.previousHash = '';
     this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
   /**
@@ -38,7 +39,31 @@ class Block {
    * @return {string} hash
    */
   calculateHash = () => {
-    return sha256(this.index + this.previousHash + this.data).toString();
+    return sha256(
+        this.index +
+        this.previousHash +
+        this.data +
+        this.nonce,
+    ).toString();
+  };
+
+  /**
+   * Mine a block - increase nonce until the hash ends with `difficulty` zeros
+   * @param {number} difficulty - number of zeros in the end
+   */
+  mineBlock = (difficulty: number) => {
+    const hashLength = this.hash.length;
+    const getLastSymbols = () => {
+      return this.hash.substring(hashLength - difficulty + 1, hashLength - 1);
+    };
+
+    let hashLastFourSymbols = getLastSymbols();
+    const zerosString = Array(difficulty + 1).join('0');
+    while (hashLastFourSymbols !== zerosString) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+      hashLastFourSymbols = getLastSymbols();
+    }
   };
 }
 
