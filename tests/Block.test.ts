e6c98e36afce7b4584ccb394bb9d84d7e92db36e
Alyona Korenkovich <al.korenkovich@mail.ru>
2023-03-20 12:24:20 +0300

    #2 Update test suits for class Block

--- tests/Block.test.ts
+++ tests/Block.test.ts
@@ -27,12 +27,57 @@ describe('Class Block', () => {
 
       // Calculated manually using a third-party service
       // eslint-disable-next-line max-len
-      const exp = '0e8f39a3a21041e31632e8db78bedaa5c2d6d9fe09f892eec4161f518d9262d5';
+      const exp = 'ddb8917045fec1d7b9013d893c7fb0521229efa47b26c04cc56792158a9bb2d3';
 
       expect(testBlock.index).toBe(109);
       expect(testBlock.data).toBe('EcSiC183e5V4MVfUdPs42n6IU');
       expect(testBlock.hash).toBe(exp);
       expect(testBlock.previousHash).toBe('');
     });
+  });
+
+  describe('Hash', () => {
+    it('should change block hash when it\'s fields has been tampered',
+        () => {
+          const testBlock = createBlock({
+            index: 109,
+            data: 'testBlock',
+          });
+          const prevTestBlockHash = testBlock.hash;
+
+          testBlock.data = 'changedData';
+          const currTestBlockHash = testBlock.calculateHash();
+
+          expect(prevTestBlockHash).not.toBe(currTestBlockHash);
+        },
+    );
+    it('should change block hash when `nonce` changes', () => {
+      const testBlock = createBlock({
+        index: 109,
+        data: 'testBlock',
+      });
+      const prevNonce = testBlock.nonce;
+      const prevHash = testBlock.hash;
+
+      testBlock.nonce = 1456;
+      const currHash = testBlock.calculateHash();
+
+      expect(prevNonce).not.toBe(testBlock.nonce);
+      expect(prevHash).not.toBe(currHash);
+    });
+  });
+
+  describe('Mine function', () => {
+    test('After mining hash of a block needs to end with `difficulty` zeros',
+        () => {
+          const testBlock = createBlock({
+            index: 109,
+            data: 'testBlock',
+          });
+          const regex = /[0]$/;
+
+          testBlock.mineBlock(1);
+          expect(testBlock.hash).toMatch(regex);
+        });
   });
 });
